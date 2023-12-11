b63141db23af4196cf5eab3459daa40eab0ee87e
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-12-11 00:10:02 -0800

    Implemented Basic Firebase Start

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import "./style.css";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+// import { initializeApp } from "firebase/app";
+// import {} from "firebase/auth";
+// import { } from "firebase/database";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import { get } from "firebase/database";
+
 const firebaseConfig = {
   apiKey: "AIzaSyChMH8UwkuT1sGWFU6hIFItPKH3StRBmLg",
   authDomain: "simple-server-game.firebaseapp.com",
@@ -17,4 +22,71 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+console.log("test");
+
+class basicClass {
+  constructor(readonly maxHealth: number, readonly attack: number) {}
+}
+
+(function () {
+  let thisPlayerId: string;
+  let thisPlayerRef: firebase.database.Reference;
+  let players = {};
+
+  let bossRef: firebase.database.Reference;
+
+  function setBoss() {
+    bossRef = firebase.database().ref(`boss`);
+    get(bossRef.child("/health")).then((snapshot) => {
+      if (snapshot.val() <= 0) {
+        console.log(snapshot.val());
+        bossRef.set({
+          health: 1000,
+        });
+      }
+    });
+  }
+
+  function initGame() {
+    const allPlayersRef = firebase.database().ref(`players`);
+
+    allPlayersRef.on("value", (snapshot) => {
+      players = snapshot.val() || {};
+    });
+
+    allPlayersRef.on("child_added", (snapshot) => {
+      const addedPlayer = snapshot.val();
+    });
+
+    allPlayersRef.on("child_removed", (snapshot) => {
+      const removedKey = snapshot.val().id;
+    });
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    if (user) {
+      thisPlayerId = user.uid;
+      thisPlayerRef = firebase.database().ref(`players/${thisPlayerId}`);
+
+      thisPlayerRef.set({
+        id: thisPlayerId,
+        class: "temp",
+        health: 100,
+      });
+
+      thisPlayerRef.onDisconnect().remove();
+    } else {
+    }
+  });
+
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch((error) => {
+      console.log(error.code, error.message);
+    });
+
+  setBoss();
+})();
