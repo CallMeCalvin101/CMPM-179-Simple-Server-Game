529922e5a48ad93afaad41c795a31b29fe66fb81
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-12-12 00:33:02 -0800

    Implemented Player Sabotaging

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import "./style.css";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/database";
-import { get } from "firebase/database";
+import { get, push, ref } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyChMH8UwkuT1sGWFU6hIFItPKH3StRBmLg",
@@ -34,29 +34,55 @@ class basicClass {
 (function () {
   let thisPlayerId: string;
   let thisPlayerRef: firebase.database.Reference;
+  let thisPlayerData = { health: 0 };
   //let players = {};
 
   let bossRef: firebase.database.Reference;
-  let bossData = { health: 0 };
+  let bossData = { health: 0, cooldown: 0 };
 
-  const playerActionButton = document.querySelector("#action1")!;
+  let playerAttacksRef: firebase.database.Reference;
 
-  playerActionButton.addEventListener("click", () => {
+  const playerAction1Button = document.querySelector("#action1")!;
+  playerAction1Button.addEventListener("click", () => {
     bossData.health -= 100;
+    bossData.cooldown -= 1;
     bossRef.update({
       health: bossData.health,
+      cooldown: bossData.cooldown,
     });
-    console.log(bossData.health);
   });
+
+  const playerAction2Button = document.querySelector("#action2")!;
+  playerAction2Button.addEventListener("click", () => {
+    pushPlayerAttack("attack", 10);
+  });
+
+  const UIChangedEvent: Event = new Event("ui-changed");
+
+  window.addEventListener("ui-changed", () => {
+    updatePlayerUI();
+    updateBossUI();
+  });
+
+  function updatePlayerUI() {
+    const playerUI = document.getElementById("player")!;
+    playerUI.innerHTML = `<strong>Your Health:</strong> ${thisPlayerData.health}`;
+  }
+
+  function updateBossUI() {
+    const bossUI = document.getElementById("boss")!;
+    bossUI.innerHTML = `<strong>Boss Health:</strong> ${bossData.health}`;
+  }
 
   function setBoss() {
     bossRef = firebase.database().ref(`boss`);
+
     get(bossRef.child("/health")).then((snapshot) => {
       bossData.health = snapshot.val();
       if (bossData.health <= 0) {
-        console.log(snapshot.val());
         bossRef.set({
           health: 1000,
+          cooldown: 10,
         });
       }
     });
@@ -64,30 +90,59 @@ class basicClass {
     bossRef.on("value", (snapshot) => {
       bossData.health = snapshot.child("health").val();
       if (bossData.health < 0) {
-        bossRef.set({
+        bossRef.update({
           health: 1000,
         });
       }
+
+      bossData.cooldown = snapshot.child("cooldown").val();
+      if (bossData.cooldown <= 0) {
+        thisPlayerData.health -= 10;
+        bossRef.update({
+          cooldown: 10,
+        });
+      }
+
+      window.dispatchEvent(UIChangedEvent);
     });
   }
 
-  /*
+  function pushPlayerAttack(type: string, value: number) {
+    const attackRef = firebase.database().ref(`playerAttacks/attacks`);
+    if (thisPlayerId) {
+      push(attackRef, [thisPlayerId, type, value]);
+    }
+  }
+
+  function parseOtherPlayerAttack(data: [string, string, number]) {
+    if (data[0] != thisPlayerId) {
+      if (data[1] == "attack") {
+        thisPlayerData.health -= data[2];
+      } else if (data[1] == "heal") {
+        thisPlayerData.health += data[2];
+      }
+    }
+  }
+
+  function setPlayers() {
+    playerAttacksRef = firebase.database().ref(`playerAttacks`);
+    playerAttacksRef.set({
+      attacks: [],
+    });
+
+    const attackRef = firebase.database().ref(`playerAttacks/attacks`);
+    attackRef.on("child_added", (snapshot) => {
+      parseOtherPlayerAttack(snapshot.val());
+      snapshot.ref.remove();
+      window.dispatchEvent(UIChangedEvent);
+    });
+  }
+
   function initGame() {
-    const allPlayersRef = firebase.database().ref(`players`);
-
-    allPlayersRef.on("value", (snapshot) => {
-      players = snapshot.val() || {};
-    });
-
-    allPlayersRef.on("child_added", (snapshot) => {
-      const addedPlayer = snapshot.val();
-    });
-
-    allPlayersRef.on("child_removed", (snapshot) => {
-      const removedKey = snapshot.val().id;
-    });
+    thisPlayerData.health = 100;
+    setBoss();
+    setPlayers();
   }
-  */
 
   firebase.auth().onAuthStateChanged((user) => {
     console.log(user);
@@ -113,5 +168,5 @@ class basicClass {
       console.log(error.code, error.message);
     });
 
-  setBoss();
+  initGame();
 })();
